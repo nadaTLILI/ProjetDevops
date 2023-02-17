53929b3325ee5e9f7c11c2803b2f708e6c9fd65c
aichaezzahra.sassi@esprit.tn <aichaezzahrasassi>
2023-02-17 03:56:09 +0100

    rest of unit test

--- src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -1,23 +1,26 @@
 package com.esprit.examen.services;
 
+import com.esprit.examen.entities.DetailFournisseur;
 import com.esprit.examen.entities.Fournisseur;
+import com.esprit.examen.entities.SecteurActivite;
+import com.esprit.examen.repositories.DetailFournisseurRepository;
 import com.esprit.examen.repositories.FournisseurRepository;
+import com.esprit.examen.repositories.SecteurActiviteRepository;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(Enclosed.class)
 class FournisseurServiceImplTest {
@@ -71,7 +74,7 @@ class FournisseurServiceImplTest {
         FournisseurServiceImpl fournisseurServiceImpl;
 
         @Test
-        public void addFournisseur_DelegatedDetailFournisseurToFournisseurRepository() {
+        public void addFournisseur_DelegatesDetailFournisseurToFournisseurRepository() {
 
             Fournisseur fournisseur = Fournisseur.builder().build();
             fournisseurServiceImpl.addFournisseur(fournisseur);
@@ -90,6 +93,200 @@ class FournisseurServiceImplTest {
             Fournisseur result = fournisseurServiceImpl.addFournisseur(fournisseur);
 
             assertThat(result.getDetailFournisseur().getDateDebutCollaboration().getDate()).isEqualTo(new Date().getDate());
+        }
+
+        @Test
+        public void addFournisseur_Fournisseur_addsFournisseurAndFournisseur() {
+
+            Fournisseur fournisseur = Fournisseur.builder().build();
+            Fournisseur result = fournisseurServiceImpl.addFournisseur(fournisseur);
+
+            assertThat(result).isEqualTo(fournisseur);
+        }
+    }
+
+    @RunWith(MockitoJUnitRunner.class)
+    public static class updateFournisseur {
+
+        @Mock
+        private DetailFournisseurRepository detailFournisseurRepository;
+
+        @Mock
+        private FournisseurRepository FournisseurRepository;
+
+        @InjectMocks
+        FournisseurServiceImpl fournisseurServiceImpl;
+
+        @Test
+        public void updateFournisseur_DelegatesDetailFournisseurToDetailFournisseurRepository() {
+
+            Fournisseur fournisseur = Fournisseur.builder()
+                    .detailFournisseur(DetailFournisseur.builder()
+                            .email("email")
+                            .matricule("matricule")
+                            .build())
+                    .build();
+
+            fournisseurServiceImpl.updateFournisseur(fournisseur);
+
+            ArgumentCaptor<DetailFournisseur> captor = ArgumentCaptor.forClass(DetailFournisseur.class);
+            verify(detailFournisseurRepository).save(captor.capture());
+
+            assertThat(captor.getValue()).isInstanceOf(DetailFournisseur.class);
+            assertThat(captor.getValue()).isEqualTo(fournisseur.getDetailFournisseur());
+        }
+
+        @Test
+        public void updateFournisseur_Fournisseur_SetsDetailFournisseur() {
+
+            Fournisseur fournisseur = Fournisseur.builder()
+                    .detailFournisseur(DetailFournisseur.builder()
+                            .email("email")
+                            .matricule("matricule")
+                            .build())
+                    .build();
+
+            Fournisseur result = fournisseurServiceImpl.updateFournisseur(fournisseur);
+
+            assertThat(result.getDetailFournisseur()).isEqualTo(fournisseur.getDetailFournisseur());
+        }
+
+        @Test
+        public void updateFournisseur_Fournisseur_SavesChangesAndReturnsFournisseur() {
+
+            Fournisseur fournisseur = Fournisseur.builder()
+                    .detailFournisseur(DetailFournisseur.builder()
+                            .email("email")
+                            .matricule("matricule")
+                            .build())
+                    .build();
+
+            Fournisseur result = fournisseurServiceImpl.updateFournisseur(fournisseur);
+
+            assertThat(result).isEqualTo(fournisseur);
+        }
+
+    }
+
+    @RunWith(MockitoJUnitRunner.class)
+    public static class deleteFournisseur {
+
+        @Mock
+        private FournisseurRepository fournisseurRepository;
+
+        @InjectMocks
+        FournisseurServiceImpl fournisseurServiceImpl;
+
+        @Test
+        public void deleteFournisseur_DelegatesFournisseurIdToFournieeurReposirtory() {
+
+            fournisseurServiceImpl.deleteFournisseur(1L);
+
+            ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+            verify(fournisseurRepository).deleteById(captor.capture());
+
+            assertThat(captor.getValue()).isEqualTo(1L);
+        }
+    }
+
+    @RunWith(MockitoJUnitRunner.class)
+    public static class retrieveFournisseur {
+
+        @Mock
+        private FournisseurRepository fournisseurRepository;
+
+        @InjectMocks
+        FournisseurServiceImpl fournisseurServiceImpl;
+
+        @Test
+        public void retrieveFournisseur_DelegatesFournisseurIdToFournissuerRepository() {
+
+            fournisseurServiceImpl.retrieveFournisseur(1L);
+
+            ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+            verify(fournisseurRepository).findById(captor.capture());
+
+            assertThat(captor.getValue()).isEqualTo(1L);
+        }
+
+        @Test
+        public void retrieveFournisseur_FournisseurId_ReturnsFournisseur() {
+            Mockito.when(fournisseurRepository.findById(any()))
+                    .thenReturn(Optional.of(Fournisseur.builder()
+                    .code("code")
+                    .build()));
+
+            fournisseurServiceImpl.retrieveFournisseur(1L);
+
+            ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+            verify(fournisseurRepository).findById(captor.capture());
+
+            assertThat(captor.getValue()).isEqualTo(1L);
+        }
+
+        @Test
+        public void retrieveFournisseur_NonExistantFournisseur_ReturnsNull() {
+            Mockito.when(fournisseurRepository.findById(any()))
+                    .thenReturn(Optional.empty());
+
+            Fournisseur result = fournisseurServiceImpl.retrieveFournisseur(1L);
+
+            assertThat(result).isNull();
+        }
+
+    }
+
+    @RunWith(MockitoJUnitRunner.class)
+    public static class assignSecteurActiviteToFournisseur {
+
+        @Mock
+        private FournisseurRepository fournisseurRepository;
+
+        @Mock
+        private SecteurActiviteRepository secteurActiviteRepository;
+
+        @InjectMocks
+        FournisseurServiceImpl fournisseurServiceImpl;
+
+        @Test
+        public void assignSecteurActiviteToFournisseur_DelegatesFournisseurIdToFournisseurReposirtory() {
+
+            Set<SecteurActivite> secteurActiviteSeteur = new HashSet<>();
+            secteurActiviteSeteur.add(SecteurActivite.builder().build());
+            //secteurActiviteSeteur.add(SecteurActivite.builder()
+            //        .codeSecteurActivite("code_secteur")
+             //       .build());
+
+            when(fournisseurRepository.findById(any()))
+                    .thenReturn(Optional.of(Fournisseur.builder()
+                    .secteurActivites(secteurActiviteSeteur)
+                    .build()));
+
+            fournisseurServiceImpl.assignSecteurActiviteToFournisseur(2L,1L);
+
+            ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+            verify(fournisseurRepository).findById(captor.capture());
+
+            assertThat(captor.getValue()).isEqualTo(1L);
+        }
+
+        @Test
+        public void assignSecteurActiviteToFournisseur_DelegatesFournisseurIdToSecteurActiviteReposirtory() {
+
+            Set<SecteurActivite> secteurActiviteSeteur = new HashSet<>();
+            secteurActiviteSeteur.add(SecteurActivite.builder().build());
+
+            when(fournisseurRepository.findById(any()))
+                    .thenReturn(Optional.of(Fournisseur.builder()
+                            .secteurActivites(secteurActiviteSeteur)
+                            .build()));
+
+            fournisseurServiceImpl.assignSecteurActiviteToFournisseur(2L,1L);
+
+            ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+            verify(secteurActiviteRepository).findById(captor.capture());
+
+            assertThat(captor.getValue()).isEqualTo(2L);
         }
     }
 }
